ea2da311b81d0591019d9bb03c9b9a006d21e8ea
Rich <piemonkey@protonmail.ch>
2023-09-08 12:28:18 +0200

    Improve feedback on exercise responses and errors

--- app/components/exercise.js
+++ app/components/exercise.js
@@ -5,6 +5,8 @@ import { tracked } from '@glimmer/tracking';
 
 export default class ExerciseComponent extends Component {
   @tracked
+  resStatus = undefined;
+  @tracked
   correctQuery = false;
 
   sparqlEndpoint =
@@ -14,12 +16,38 @@ export default class ExerciseComponent extends Component {
       : '');
   validQuery = this.args.validQuery;
 
+  get resStatusGood() {
+    return (
+      (this.resStatus >= 200 && this.resStatus < 300) || this.resStatus === 304
+    );
+  }
+  get errorStatus() {
+    return this.resStatus !== undefined && !this.resStatusGood;
+  }
+  get equivalentQuery() {
+    return (
+      this.resStatus !== undefined && this.resStatusGood && this.correctQuery
+    );
+  }
+  get nonEquivalentQuery() {
+    return (
+      this.resStatus !== undefined && this.resStatusGood && !this.correctQuery
+    );
+  }
+
+  @action
+  elementInserted(element) {
+    this.element = element;
+  }
+
   @action
   async onQueryEventHandler(_instance, tab) {
+    this.resStatus = undefined;
     this.correctQuery = false;
     if (this.validQuery === undefined) {
       return;
     }
+    window.scrollTo({ top: this.element.offsetTop, behavior: 'smooth' });
 
     const query = tab.getQuery();
     const response = await fetch(
@@ -31,9 +59,12 @@ export default class ExerciseComponent extends Component {
         headers: new Headers({ Accept: 'application/vnd.api+json' }),
       }
     );
-    const json = await response.json();
-    if (json.data) {
-      this.correctQuery = json.data.attributes.equivalent;
+    this.resStatus = response.status;
+    if (response.ok) {
+      const json = await response.json();
+      if (json.data) {
+        this.correctQuery = json.data.attributes.equivalent;
+      }
     }
   }
 }
